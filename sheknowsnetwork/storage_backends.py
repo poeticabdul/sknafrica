bf7ce947ea61f9a9a5c80a4ae382d96a93d05600
abdul <poeticabdul@gmail.com>
2020-09-11 23:58:10 +0000

    se challenges

--- sheknowsnetwork/storage_backends.py
+++ sheknowsnetwork/storage_backends.py
@@ -2,11 +2,14 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = 'static'
+    default_acl = 'public-read'
 
 class MediaStorage(S3Boto3Storage):    
     location = 'media'    
     file_overwrite = False
+    default_acl = 'public-read'
 
 class PublicMediaStorage(S3Boto3Storage):
     location = 'media/public'
     file_overwrite = False
+    default_acl = 'public-read'
